6aebf4df79c9459f190445ddea55b8b08913daa4
zhulicious <azul_zulis@hotmail.com>
2016-02-11 00:19:45 +0100

    klar level, utan NPC + AI

--- AndraMig/Assets/Script/Point.cs
+++ AndraMig/Assets/Script/Point.cs
@@ -5,34 +5,49 @@ public class Point : MonoBehaviour {
 
     public disenableUi uiScript;
 
+    //private bool taken;
+    private Vector3 player;
+
     public void Start ()
     {
         uiScript = GameObject.Find("Buddy").GetComponent<disenableUi>();
+        //taken = false;
     }
 
-	void Update()
-	{
-		transform.rotation = transform.rotation * Quaternion.Euler(Vector3.up);
-	}
+	//void Update()
+	//{
+	//	//transform.rotation = transform.rotation * Quaternion.Euler(Vector3.up);
+ //       if (taken)
+ //       {
+ //           transform.position = Vector3.MoveTowards(transform.position, player, Time.deltaTime * 10);
+ //           if (transform.position == player)
+ //               Destroy(gameObject);
+ //       }
+	//}
 	
-	void OnTriggerStay(Collider col)
+	void OnTriggerEnter (Collider col)
 	{
 		if(col.gameObject.tag == "Player")
 		{
-            Vector3 player = col.GetComponent<Collider>().bounds.center;
-			transform.position = Vector3.MoveTowards(transform.position, player, Time.deltaTime * 10);
-            if (transform.position == player)
-                Destroy(gameObject);
-                
-		}
-	}
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.tag == "Player")
-        {
-            uiScript.point++;
+            player = col.GetComponent<Collider>().bounds.center;
+            ////taken = true;
+            //uiScript.point++;
             uiScript.SendMessage("TurnItOn");
             uiScript.Invoke("ShutItOff", 5);
+
+            transform.position = Vector3.MoveTowards(transform.position, player, Time.deltaTime * 10);
+            if (transform.position == player)
+                Destroy(gameObject);
+            uiScript.point++;
+
         }
-    }
+	}
+    //void OnTrigger(Collider col)
+    //{
+    //    if (col.gameObject.tag == "Player")
+    //    {
+    //        uiScript.SendMessage("TurnItOn");
+    //        uiScript.Invoke("ShutItOff", 5);
+    //    }
+    //}
 }
